44f48c4d6c3f03f26d13c4c672a900b23a44ba25
FranBC04 <124638544+FranBC04@users.noreply.github.com>
2023-02-08 01:01:45 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+document.getElementById("face").onclick = function () {
+    document.getElementById("nombreface").innerHTML = "Marc Turner13";
+    
+}
+
+document.getElementById("insta").onclick = function () {
+    document.getElementById("nombreinsta").innerHTML = "marcturner13";
+    
+}
+
+document.getElementById("gmail").onclick = function () {
+    document.getElementById("nombregmail").innerHTML = "marcturner@gmail.com";
+}
